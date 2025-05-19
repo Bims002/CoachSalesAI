8d7ef2f4ac1c3db98c38fc01bbe427b7241ac084
Bims <bjbimeme@gmail.com>
2025-05-19 13:29:35 +0100

    Add Hotjar tracking code to public/index.html

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,26 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const hotjarScript = `
+<!-- Hotjar Tracking Code for Coachsales -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj||[]).push(arguments)};
+        h._hjSettings={hjid:6408029,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+`;
+
+const head = document.getElementsByTagName('head')[0];
+const script = document.createElement('script');
+script.type = 'text/javascript';
+script.innerHTML = hotjarScript;
+head.appendChild(script);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
